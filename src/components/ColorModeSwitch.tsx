fa3aab7476bd8dbbaf1d3777e9164f316f016513
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-06-11 12:07:02 +0100

    Update color mode switch label

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,14 +1,20 @@
-import { HStack, Switch } from "@chakra-ui/react";
+import { HStack, Switch, Text } from "@chakra-ui/react";
 import { useColorMode } from "@/components/ui/color-mode";
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack align="center" justify="flex-end" w="100%">
-      <Switch.Root colorScheme='green' checked={ colorMode === 'dark' } onChange={toggleColorMode}>
+      <Switch.Root
+        colorScheme="green"
+        checked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      >
         <Switch.HiddenInput />
         <Switch.Control />
-        <Switch.Label>Dark Mode</Switch.Label>
+        <Switch.Label>
+          <Text textTransform="capitalize">{colorMode} Mode</Text>
+        </Switch.Label>
       </Switch.Root>
     </HStack>
   );

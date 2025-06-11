68f31102d30cf2d697bd3d4a4946116ef8741378
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-06-11 06:52:53 +0100

    styles update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
+import Navbar from "@/components/Navbar";
 
 function App() {
   return (
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav" display='flex' alignItems='center'>
+        <Navbar />
+        <Text>Navbar</Text>
       </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
-      </Show>
+      <GridItem area="aside" bg="gold" hideBelow='lg'>
+        Aside
+      </GridItem>
       <GridItem area="main" bg="dodgerblue">
         Main
       </GridItem>

032b2290702b6ba73d7784b07749546782f6bd4a
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-06-11 13:08:19 +0100

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root borderRadius={10} overflow="hidden">
+    <Card.Root borderRadius={10} overflow="hidden" width="300px">
       <Image
         src={getCroppedImageUrl(game.background_image)}
         alt="Game thumbnail"

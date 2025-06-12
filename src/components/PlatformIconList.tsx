f7fae772efde552b313105e487803c5937d9085c
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-06-12 14:43:09 +0100

    Fix: Missing icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -9,6 +9,7 @@ import {
   FaAndroid,
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
+import { PiShootingStar } from "react-icons/pi";
 import {
   SiNintendo,
   SiPlaystation2,
@@ -16,6 +17,8 @@ import {
   SiPlaystation4,
   SiPlaystation5,
   SiSega,
+  SiCommodore,
+  SiAtari
 } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import type { IconType } from "react-icons";
@@ -31,22 +34,32 @@ const PlatformIconList = ({ platforms }: PlatformIconListProps) => {
     playstation4: SiPlaystation4,
     playstation3: SiPlaystation3,
     playstation2: SiPlaystation2,
+    playstation1: FaPlaystation,
     "ps-vita": FaPlaystation,
     xbox: FaXbox,
     "xbox-one": FaXbox,
     "xbox-old": FaXbox,
     xbox360: FaXbox,
     "xbox-series-x": FaXbox,
+    "xbox-series-s": FaXbox,
     ios: MdPhoneIphone,
     mac: FaApple,
     macos: FaApple,
     linux: FaLinux,
     android: FaAndroid,
     nintendo: SiNintendo,
+    wii: SiNintendo,
     "nintendo-switch": SiNintendo,
+    "game-boy": SiNintendo,
+    "game-boy-advance": SiNintendo,
+    commodore: SiCommodore,
+    "commodore-amiga": SiCommodore,
     web: BsGlobe,
     dreamcast: SiSega,
     sega: SiSega,
+    genesis: PiShootingStar,
+    atari: SiAtari,
+    "atari-st": SiAtari,
   };
   return (
     <HStack marginY={4}>

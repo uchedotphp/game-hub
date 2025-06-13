d132c097206492683b92bdef25872508fd12d718
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-06-13 05:23:27 +0100

    Move sensitive keys to env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: 'https://api.rawg.io/api',
+    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: 'https://api.rawg.io/api',
     // timeout: 10000,
     // headers: {
     //     'Content-Type': 'application/json',
     // },
     params: {
-        key: 'd0f017ac8e8545ec82ac242ac2d7f91e'
+        key: import.meta.env.VITE_API_KEY
     }
 })
